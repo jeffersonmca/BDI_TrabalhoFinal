a4c56fdf7a9f88cf02cc8cf354507af17136b08e
Gabriel Rodrigues <gabrielws31@gmail.com>
2018-11-27 18:48:48 -0200

    3 selects feitos

--- Selects.sql
+++ Selects.sql
@@ -5,11 +5,15 @@ WHERE Cursinho.aluno_curso.curso_idcurso = Cursinho.curso.idcurso AND
 	  Cursinho.curso.idcurso = 1;
 
 #Consulta que retorna quantos professores estao cadastrados no DB #
-SELECT
-FROM
-WHERE Cursinho.Funcionario
-
+SELECT COUNT(funcionario.cargo_idCargo) AS Quantidade
+FROM Cursinho.funcionario JOIN Cursinho.cargo
+WHERE Cursinho.cargo.idcargo=Cursinho.funcionario.cargo_idCargo AND Cursinho.cargo.nome='Professor';
 #Consulta que retorna o endereco dos alunos que estao em debito com a mensalidade #
+SELECT nome,rua,numero,bairro,cidade
+FROM endereco JOIN pessoa JOIN aluno JOIN contas_A_pagar
+WHERE Cursinho.endereco.idendereco=Cursinho.pessoa.endereco_idendereco 
+	AND Cursinho.pessoa.idpessoa=Cursinho.aluno.pessoa_idpessoa 
+    AND Cursinho.aluno.idaluno=Cursinho.contas_A_pagar.aluno_idaluno;
 #Consulta que retorna a quantidade de funcionarios em cada cargo #
 #Consulta que retorna os alunos que estao em uma determinada turma cadastrada #
 #Consulta que retorna a quantidade de pessoas de uma determinada cidade #
